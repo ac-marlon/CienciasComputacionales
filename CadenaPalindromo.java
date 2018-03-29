edd2e8d42eb9eb11baa5640b22bc7cef0ea48ab5
ac-marlon <acmarlon@outlook.com>
2018-03-29 14:29:13 -0500

    Convierte una palabra en palíndromo usando inserciones y sustituciones

--- CadenaPalindromo.java
+++ CadenaPalindromo.java
@@ -4,28 +4,26 @@
  * and open the template in the editor.
  */
 
-package alternativo;
+package cadenaspalindromo;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  *
- * @author FamiliaAC
+ * @author AC-Marlon
  */
 
-public class Alternativo {
+public class CadenasPalindromo {
 
     public static void main(String[] args) {
-
         Scanner s = new Scanner(System.in);
         String cadena;
         ArrayList<String> letras1 = new ArrayList<>();
         ArrayList<String> letras2 = new ArrayList<>();
-        ArrayList<String> palindromo1 = new ArrayList<>();
-        ArrayList<String> palindromo2 = new ArrayList<>();
+        ArrayList<String> palindromo1;
+        ArrayList<String> palindromo2;
         cadena = s.nextLine();
-
         if (esPalindromo(cadena)) {
         } else {
             for (int i = 0; i < cadena.length(); i++) {
@@ -34,17 +32,12 @@ public class Alternativo {
             }
         }
         palindromo1 = sustitucionesPalindromo(letras1);
-        palindromo2 = insercionesPalindromo(letras2);
-
-        if (palindromo1.size() < palindromo2.size()) {
-            System.out.println("Caso Optimo: " + palindromo1);
-        } else {
-            System.out.println("Caso Optimo: " + palindromo2);
-        }
+        palindromo2 = insercionesPalindromo(letras2);     
+        System.out.println("Palindromo por sustituciones : " + palindromo1);
+        System.out.println("Palindromo por inserciones : " + palindromo2);
     }
 
     public static boolean esPalindromo(String cadena) {
-
         String aux = "";
         for (int i = cadena.length() - 1; i >= 0; i--) {
             aux = aux + cadena.charAt(i);
@@ -53,30 +46,20 @@ public class Alternativo {
     }
 
     private static ArrayList<String> sustitucionesPalindromo(ArrayList<String> letras) {
-
-        int cont = 0;
         for (int i = 1; i < letras.size(); i++) {
             if (!letras.get(i - 1).equals(letras.get(letras.size() - i))) {
                 letras.set(letras.size() - i, letras.get(i - 1));
-                cont++;
             }
         }
-        //System.out.println(cont);
         return letras;
     }
 
     private static ArrayList<String> insercionesPalindromo(ArrayList<String> letras) {
-
-        int cont = 0;
         for (int i = 1; i < letras.size(); i++) {
             if (!letras.get(i - 1).equals(letras.get(letras.size() - i))) {
                 letras.add(letras.size() - i + 1, letras.get(i - 1));
-                cont++;
             }
         }
-        //System.out.println(cont);
         return letras;
     }
-
 }
-
